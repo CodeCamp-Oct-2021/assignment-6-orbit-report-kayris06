12a2811e7b846582c1cb31b821ed204cffeea8c4
Krista Valdez <kayris06@gmail.com>
2021-11-15 16:03:18 -0600

    assignment6

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -6,18 +6,17 @@ export class Satellite {
 	orbitType: string;
 	operational: boolean;
 
+
 	constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
 		this.name = name;
 		this.type = type;
 		this.launchDate = launchDate;
 		this.orbitType = orbitType;
 		this.operational = operational;
-   }
-	
+	}
+
 	isSpaceDebris(): boolean {
-		return true;
-   }
-
+		return (this.type === 'Space Debris');
+	}
 }
-
 // TODO 3a: fix isSpaceDebris check
